6adddeed49237ff55c35dea9210c380d310468df
CesarESierraJ <23221025@unamad.edu.pe>
2026-08-09 17:46:22 -0500

    actualizar numero de whatsapp XD

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="https://api.whatsapp.com/send?phone=51987041617&text=Hola,%20quisiera%20información."
+      href="https://api.whatsapp.com/send?phone=51931921943&text=Hola,%20quisiera%20información."
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 bg-[#25D366] hover:bg-[#1ebe5d] text-white p-4 rounded-full shadow-xl transition hover:scale-110"
